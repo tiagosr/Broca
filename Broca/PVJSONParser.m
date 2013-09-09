50ea013caa892c1a585899b3c9148ea244dfd9f7
Tiago Rezende <tiagosr@gmail.com>
2013-09-09 04:39:11 -0300

    "finished" JSON parser
    can't get myself to test it right now, must sleep.

--- Broca/PVJSONParser.m
+++ Broca/PVJSONParser.m
@@ -22,6 +22,7 @@
 #import "PVLiteral.h"
 #import "PVZeroOrMore.h"
 #import "PVForward.h"
+#import "PVRegex.h"
 
 
 @implementation PVJSONParser
@@ -30,15 +31,25 @@
 {
     PVIgnore *_ = [PVIgnore :[PVZeroOrMore :[PVCharacter inString:@" \t\n\r"]]];
     PVForward *dict = [PVForward forward];
-    
-    
-    PVSequence *key_value = [PVSequence :_,
-                             [PVLiteral :@":"], _, _];
-    PVSequence *key_value_comma = [PVSequence :key_value, _, [PVLiteral :@","], _];
+    PVForward *array = [PVForward forward];
+    PVRegex *string = [PVRegex :@"\"([^\"\\\\]*|\\\\[\"\\\\bfnrt/]|\\\\u[0-9a-f]{4}))*\"" :0];
+    PVRegex *symbol = [PVRegex :@"[a-zA-Z_$][a-zA-Z_$0-9]*" :0];
+    PVOrderedChoice *keyword = [PVOrderedChoice :[PVLiteral :@"true"], [PVLiteral :@"false"], [PVLiteral :@"null"]];
+    PVRegex *number = [PVRegex :@"-?(?=[1-9]|0(?!\\d))\\d+(\\.\\d+)?([eE][+-]?\\d+)?" :0];
+    PVIgnore *separator_comma = [PVIgnore :[PVLiteral :@","]];
+    PVOrderedChoice *value = [PVOrderedChoice :dict, array, string, keyword, number];
+    PVSequence *value_comma = [PVSequence :value, _, separator_comma];
+    PVSequence *key_value = [PVSequence :_, [PVOrderedChoice :symbol, string], _,
+                             [PVLiteral :@":"], _, value, _];
+    PVSequence *key_value_comma = [PVSequence :key_value, _, separator_comma, _];
+
+    PVSequence *farray = [PVSequence :[PVLiteral :@"["], _, [PVZeroOrMore :value_comma], [PVOptional :value], [PVLiteral :@"]"]];
     PVSequence *fdict = [PVSequence :_, [PVLiteral :@"{"],
                                     [PVZeroOrMore :key_value_comma],
                                     [PVOptional :key_value],
                                     [PVLiteral :@"}"], _];
+
+    array.forwarded = farray;
     dict.forwarded = fdict;
     PVName * start = [PVName :@"start" :dict];
     return start;
